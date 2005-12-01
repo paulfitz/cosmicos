327b8acde5648b3545358506d93db0932c528157
Paul Fitzpatrick <paul@robotrebuilt.com>
2005-12-01 23:45:40 +0000

    actually does something now

--- tools-src/whitespace.cpp
+++ tools-src/whitespace.cpp
@@ -26,15 +26,58 @@ public:
 };
 
 
+enum {
+  FAMILY_UNIT,
+  FAMILY_BODIED,
+  FAMILY_APPLY,
+};
+
+
+class Emit {
+private:
+  int prev_at;
+  ostream& os;
+public:
+
+  Emit(ostream& nos) : os(nos) { prev_at = 0; }
+
+  void write(const char *str) {
+    write(str,prev_at);
+  }
+
+  void write(const char *str, int at) {
+    //printf("asked to write [%s]\n", str);
+    if (at<prev_at) {
+      os << endl;
+      prev_at = 0;
+    }
+    while (prev_at<at && prev_at>=0) {
+      os << " ";
+      prev_at++;
+      if (prev_at>200) {
+	printf("seeking to %d from %d for [%s]\n", at, prev_at, str);
+	exit(1);
+      }
+    }
+    int len = strlen(str);
+    os << str;
+    prev_at += len;
+  }
+};
+
 class Exp {
 private:
   bool auto_paren;
   bool commentary;
   bool terminated;
   bool bag;
+  bool name_set;
   list<Exp> sub;
   Exp *tparent;
   string content;
+  string render;
+  int pos;
+  int len;
 
 public:
   Exp(Exp *nparent = NULL) {
@@ -42,6 +85,7 @@ public:
     commentary = false;
     terminated = false;
     bag = false;
+    name_set = false;
     tparent = nparent;
     content = "";
   }
@@ -53,6 +97,7 @@ public:
     bag = alt.bag;
     tparent = alt.tparent;
     content = alt.content;
+    name_set = alt.name_set;
   }
 
   Exp *add() {
@@ -75,6 +120,11 @@ public:
 
   void setName(const char *name) {
     content = name;
+    name_set = true;
+  }
+
+  const char *getName() {
+    return content.c_str();
   }
 
   void setAutoParen(bool flag) {
@@ -93,9 +143,118 @@ public:
     bag = flag;
   }
 
+  int getFamily() {
+    int family = FAMILY_UNIT;
+    if (!name_set) {
+      family = FAMILY_APPLY;
+      if (sub.size()>=1) {
+	if (strcmp(first().getName(),"define")==0) {
+	  family = FAMILY_BODIED;
+	}
+      }
+    }
+    return family;
+  }
+
+  void layout() {
+    if (!name_set) {
+      pos = 0;
+      int pre_len = 0;
+      int call_len = 0;
+      int longest_arg_len = 0;
+      int last_len = 0;
+      int first = 1;
+      int arg_ct = 0;
+      int speedy = 0;
+      for (list<Exp>::iterator it = sub.begin(); it!=sub.end(); it++) {
+	(*it).layout();
+	int ilen = (*it).len;
+	pre_len += ilen;
+	last_len = ilen;
+	if (!first) {
+	  if (ilen>longest_arg_len) {
+	    longest_arg_len = ilen;
+	  }
+	} else {
+	  call_len = ilen;
+	  if (!(*it).name_set) {
+	    speedy = 1;
+	    printf("ZAP call_len for: \n");
+	    (*it).write(cout);
+	    call_len = -1;
+	    longest_arg_len = ilen;   
+	  }
+	}
+	first = 0;
+	arg_ct++;
+      }
+      if (!bag) {
+	int ap = auto_paren?1:0;
+	if (pre_len+2<20 || (arg_ct<=2&&!speedy)) {
+	  int at = 1;
+	  for (list<Exp>::iterator it = sub.begin(); it!=sub.end(); it++) {
+	    (*it).pos = at+ap;
+	    at += (*it).len+1;
+	  }
+	  if (at<=1) at=2;
+	  len = at;
+	} else {
+	  for (list<Exp>::iterator it = sub.begin(); it!=sub.end(); it++) {
+	    if (it!=sub.begin()) {
+	      (*it).pos = call_len+2+ap;
+	    } else {
+	      (*it).pos = 1+ap;
+	    }
+	  }
+	  len = call_len+1+last_len+2;
+	}
+      } else {
+	len = 0;
+	pos = 0;
+      }
+    } else {
+      pos = 0;
+      len = content.length();
+    }
+  }
+
+
+  void layout(ostream& os) {
+    layout();
+    Emit emit(os);
+    layout_helper(emit,0);
+    os << endl;
+  }
+  
+  void layout_helper(Emit& emit, int at) {
+    if (name_set) {
+      emit.write(content.c_str(),at+pos);
+    } else {
+      if (!bag && !auto_paren) {
+	emit.write("(",at+pos);
+      }
+      int last_pos = at+pos+1;
+      for (list<Exp>::iterator it = sub.begin(); it!=sub.end(); it++) {
+	if ((*it).auto_paren) {
+	  emit.write(" /");
+	}
+	(*it).layout_helper(emit,at+pos);
+	last_pos = (*it).pos+(*it).len+at+pos;
+      }
+      if (!bag) {
+	if (!auto_paren) {
+	  emit.write(")",last_pos);
+	}
+	if (terminated) {
+	  emit.write(";",last_pos+1);
+	}
+      }
+    }
+  }
+
   void write(ostream& os) {
     bool need_space = false;
-    if (sub.size()>=1) {
+    if (!name_set) {
       if (!bag) {
 	if (!auto_paren) {
 	  os << "(";
@@ -157,7 +316,6 @@ public:
 
   void applyLeft() {
     current = current->add();
-    current->setName("BLANK");
   }
 
   void applyRight() {
@@ -183,9 +341,10 @@ public:
     applySymbol(name,true);
   }
 
+
   void write(ostream& os) {
     base.setBag(true);
-    base.write(os);
+    base.layout(os);
   }
 };
 
@@ -207,6 +366,9 @@ void ExpBuilder::apply(char ch) {
       case '(':
       case ')':
       case ' ':
+      case '\t':
+      case '\n':
+      case '\r':
       case '/':
       case ';':
 	applySymbol(symbol_text.c_str());
@@ -234,6 +396,7 @@ void ExpBuilder::apply(char ch) {
       break;
     case '\n':
     case '\r':
+    case '\t':
     case ' ':
       break;
     default:
@@ -257,12 +420,10 @@ void ExpBuilder::apply(const Text& text) {
 
 
 int main() {
-  cerr << "This program basically does nothing right now.  If you feed in a ftz file, it regenerates it" << endl;
   ExpBuilder exp;
   string in;
   while (!(cin.eof()||cin.bad())) {
     getline(cin,in);
-    //cout << "INPUT IS <" << in << ">" << endl;
     in += "\n";
     exp.apply(Text(in.c_str()));
   }
