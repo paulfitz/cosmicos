be74f39e19c6ce672e32ada09455c3ffed8e5200
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-03-31 04:23:05 +0200

    fix use of node on clean build

--- transform/cmake/node.cmake
+++ transform/cmake/node.cmake
@@ -1,4 +1,3 @@
 include(setup.cmake)
 set(ENV{NODE_PATH} "${out_dir}")
-message(STATUS "set node path to $ENV{NODE_PATH}")
-execute_process(COMMAND ${NODE} ${in} ${args})
+execute_process(COMMAND ${NODE} ${in} ${nargs} OUTPUT_FILE ${out_dir}/${log}.txt)
