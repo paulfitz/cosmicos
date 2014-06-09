577cb2d0a1e1487de7cd0a8b0e0d7de8504f59d8
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-06-09 22:56:19 +0000

    call jar correctly, fixes #1

--- transform/cmake/convert_gate.cmake
+++ transform/cmake/convert_gate.cmake
@@ -1,6 +1,6 @@
 include(setup.cmake)
 
-execute_process(COMMAND ${JAVA_EXE} -jar ${UNLESS} ${in} OUTPUT_FILE ${out_dir}/${out}.base WORKING_DIRECTORY ${base})
+execute_process(COMMAND ${JAVA_EXE} -cp "${UNLESS}" UnlessDriver ${in} OUTPUT_FILE ${out_dir}/${out}.base WORKING_DIRECTORY ${base})
 execute_process(COMMAND perl ${base}/../bin/drawgate-ppm.pl INPUT_FILE ${out}.base OUTPUT_FILE ${name}.ppm)
 execute_process(COMMAND perl ${base}/../bin/drawgate-txt.pl INPUT_FILE ${out}.base OUTPUT_FILE ${out}.text)
 
