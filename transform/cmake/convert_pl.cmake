cf48fc4a078a1e27aa1e8e7c4fff6b1e3e1940be
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-08-28 03:43:11 +0200

    catch perl evaluation errors

--- transform/cmake/convert_pl.cmake
+++ transform/cmake/convert_pl.cmake
@@ -1,2 +1,9 @@
 include(setup.cmake)
-execute_process(COMMAND perl -I${base} ${base}/${in} OUTPUT_FILE ${out})
+execute_process(COMMAND perl -I${base} ${base}/${in} OUTPUT_FILE ${out} RESULT_VARIABLE result)
+
+if (NOT ${result} EQUAL 0)
+  if (EXISTS ${out})
+    file(REMOVE ${out})
+  endif()
+  message(FATAL_ERROR "perl returned ${result}")
+endif()
