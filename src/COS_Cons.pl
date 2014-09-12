c404befcd3cc007cb6f56ad31e17006cb1bb9d56
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2014-09-12 05:21:29 +0200

    clean up pairs lesson

--- src/COS_Cons.pl
+++ src/COS_Cons.pl
@@ -7,9 +7,9 @@ sub ShowTheLesson {
   my $txt = "";
   $txt .= "# MATH illustrate pairs\n";
 
-  $txt .= "(define cons (? x / ? y / ? f / f (x) (y)));\n";
-  $txt .= "(define car (? pair / pair (? x / ? y / x)));\n";
-  $txt .= "(define cdr (? pair / pair (? x / ? y / y)));\n";
+  $txt .= "define cons | ? x | ? y | ? z | z \$x \$y;\n";
+  $txt .= "define car | ? cons:z | cons:z | ? x | ? y \$x;\n";
+  $txt .= "define cdr | ? cons:z | cons:z | ? x | ? y \$y;\n";
 
   {
       my $idx = 0;
@@ -17,8 +17,8 @@ sub ShowTheLesson {
       for (my $i=0; $i<3; $i++) {
 	  my $x1 = $examples[$idx];  $idx++;
 	  my $x2 = $examples[$idx];  $idx++;
-	  $txt .= "(assign x (cons $x1 $x2) / = (car / x) $x1);\n";
-	  $txt .= "(assign x (cons $x1 $x2) / = (cdr / x) $x2);\n";
+	  $txt .= "assign x (cons $x1 $x2) | = $x1 | car \$x;\n";
+	  $txt .= "assign x (cons $x1 $x2) | = $x2 | cdr \$x;\n";
       }
   }
   {
@@ -28,27 +28,25 @@ sub ShowTheLesson {
 	  my $x1 = $examples[$idx];  $idx++;
 	  my $x2 = $examples[$idx];  $idx++;
 	  my $x3 = $examples[$idx];  $idx++;
-	  $txt .= "(assign x (cons $x1 / cons $x2 $x3) / = (car / x) $x1);\n";
-	  $txt .= "(assign x (cons $x1 / cons $x2 $x3) / = (car / cdr / x) $x2);\n";
-	  $txt .= "(assign x (cons $x1 / cons $x2 $x3) / = (cdr / cdr / x) $x3);\n";
+	  $txt .= "assign x (cons $x1 | cons $x2 $x3) | = $x1 | car \$x;\n";
+	  $txt .= "assign x (cons $x1 | cons $x2 $x3) | = $x2 | car | cdr \$x;\n";
+	  $txt .= "assign x (cons $x1 | cons $x2 $x3) | = $x3 | cdr | cdr \$x;\n";
       }
       {
 	  my @examples = prand(5,5);
 	  my @pre = @examples[0..($#examples-1)];
 	  my $post = $examples[$#examples];
-	  $txt .= "(assign x (cons " . join(" / cons ",@pre) . " $post) ";
-	  for (my $k=0; $k<$#examples; $k++) {
-	      $txt .= "/ and (= $pre[$k] / car / ";
-	      for (my $i=0; $i<$k; $i++) {
-		  $txt .= "cdr / ";
+	  for (my $k=0; $k<=$#examples; $k++) {
+	      $txt .= "assign x (cons " . join(" | cons ",@pre) . " $post) ";
+	      $txt .= "| = $examples[$k]";
+	      if ($k<$#examples) {
+		  $txt .= " | car";
 	      }
-	      $txt .= "x) ";
+	      for (my $i=0; $i<$k; $i++) {
+		  $txt .= " | cdr";
+	      }
+	      $txt .= " \$x;\n";
 	  }
-	  $txt .= "(= $post / ";
-	  for (my $i=0; $i<$#examples; $i++) {
-	      $txt .= "cdr / ";
-	  }
-	  $txt .= "x));\n";
       }
   }
 
