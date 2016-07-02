f5e2d99670b158d7ae459ea95112569f5d93a552
joha2 <joha2@gmx.net>
2016-07-02 15:24:36 +0200

    it is possible to compare texts with their word/frequency content graphicallt

--- src/decoding_cosmicos.py
+++ src/decoding_cosmicos.py
@@ -32,7 +32,22 @@ class DecoderClass(object):
         preliminary = [str(i) for i in list(np.random.random_integers(0, 3, (limit,)))]
         self.origmsgtext = ''.join(preliminary)
         self.msgtext = self.origmsgtext
-        return limit
+        return (limit, self.msgtext)
+        
+    def readStandardTextFromFile(self, filename, limit=10000):
+        print('---------')
+        print("Reading text from file %s with limit %d characters" % (filename, limit))
+        
+        fl = open(filename,'r')
+        s = fl.read()
+        s.lower()
+        fl.close()
+        if limit > 0:
+            s = s[0:limit]
+        lenmsg = len(s)
+        print('---------')
+        return (lenmsg, s)
+        
     
     def readMessage(self, filename, limit=10000):
         print('---------')
@@ -47,16 +62,12 @@ class DecoderClass(object):
         lenmsg = len(self.msgtext)
         print('---------')
         return lenmsg
-
-    def doesItObeyZipfsLaw(self, delimsymbols):
         
-        print("Printing word frequency over ordered by frequency rank.")
-        print("This obviously relies on the correct choice of delimiter symbols.")
-        print("This should give a power law according to Zipf\'s law.")        
         
-        modifiedmsg = re.sub(delimsymbols, ' ', self.msgtext)
+    def performFrequencyRankOrderingAndFit(self, msgtext, delimsymbols, wordre):
+        modifiedmsg = re.sub(delimsymbols, ' ', msgtext)
         lenmodifiedmsg = len(modifiedmsg)
-        pwords = re.compile(r'[01]+') # usually \w+ but we have digits instead of letters
+        pwords = re.compile(wordre) # usually \w+ but we have digits instead of letters
         wordlist = pwords.findall(modifiedmsg)
 
         worddict = {}
@@ -75,13 +86,16 @@ class DecoderClass(object):
         log10freqranking = np.log10(freqranking)
         
         [decreasing, intersection] = np.lib.polynomial.polyfit(log10freqranking[:,0],log10freqranking[:,1],1)
-        
-        # y = a*x^b
-        # log10 y = log10 a + b*log10 x
 
-        xfit = np.linspace(freqranking[0, 0], freqranking[-1, 0], 100)
-        yfit = 10.0**intersection*np.power(xfit, decreasing)
+        return (freqranking, decreasing, intersection)
+
+
+    def doesItObeyZipfsLaw(self, textlist, delimiterlist, wordrelist, colorlist_points, colorlist_fits):
         
+        print("Printing word frequency over ordered by frequency rank.")
+        print("This obviously relies on the correct choice of delimiter symbols.")
+        print("This should give a power law according to Zipf\'s law.")        
+
         fig = plt.figure(1)
         ax = fig.add_subplot(111)
 
@@ -90,13 +104,30 @@ class DecoderClass(object):
 
         ax.set_yscale('log')
         ax.set_xscale('log')
-        
-        ax.set_title('Zipfs law y = a*x^b: a = '
-            +str(10.0**intersection) + ' b = ' + str(decreasing))
+
         ax.set_xlabel('rank # according to frequency (decreasing len frequency)')
         ax.set_ylabel('word frequency')
+ 
+        texts_to_analyse = [self.msgtext] + textlist       
+        delimiters_to_use = [r'[23]+'] + delimiterlist
+        wordres_to_use = [r'[01]+'] + wordrelist
+        colorlist_points_to_use = ['r'] + colorlist_points
+        colorlist_fits_to_use = ['r'] + colorlist_fits
         
-        ax.plot(freqranking[:, 0], freqranking[:, 1], 'r.', xfit, yfit, 'b')
+        for (text, delimiters, wordre, color_points, color_fits) in zip(texts_to_analyse, delimiters_to_use, wordres_to_use, colorlist_points_to_use, colorlist_fits_to_use):
+            
+            (freqranking, decreasing, intersection) = self.performFrequencyRankOrderingAndFit(text, delimiters, wordre)        
+        
+            # y = a*x^b
+            # log10 y = log10 a + b*log10 x
+
+            xfit = np.linspace(freqranking[0, 0], freqranking[-1, 0], 100)
+            yfit = 10.0**intersection*np.power(xfit, decreasing)
+        
+       
+            ax.set_title('Zipf\'s Law y = a*x^b')
+        
+            ax.plot(freqranking[:, 0], freqranking[:, 1], color_points+'.', xfit, yfit, color_fits)
 
         plt.show()
 
@@ -271,9 +302,13 @@ def main(argv):
 
     d = DecoderClass()    
     
-    #msglen = d.generateRandomMessage(limit=300000)
+    (msglen, randomtext) = d.generateRandomMessage(limit=600000)
+    #(txtlen, mobytext) = d.readStandardTextFromFile("../moby_dick.txt", limit=0)
     msglen = d.readMessage(argv[1], limit=0)
-    d.doesItObeyZipfsLaw('[23]+')
+    
+    d.doesItObeyZipfsLaw([randomtext], [r'[23]+'], [r'[01]+'], ['g'], ['g'])
+    # check various texts or messages for their ranked frequency content    
+    
     d.showGraphicalRepresentation(width=512)
 
     # message at the actual version is somehow not correctly encoded
