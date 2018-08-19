762de98793775539880e0adbfcbaede641b53e25
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2018-08-18 21:31:11 -0400

    fix up cosmsg path to work without fuss from host after docker build

--- tools/command_line/cosmsg.js
+++ tools/command_line/cosmsg.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
-var cos = require('@CMAKE_SOURCE_DIR@/transform/assemble/cosmicos.js');
-return cos('@CMAKE_BINARY_DIR@','@CMAKE_SOURCE_DIR@');
+var path = require('path');
+
+var binaryDir = path.resolve(__dirname, '..');
+var delta = path.relative('@CMAKE_BINARY_DIR@', '@CMAKE_SOURCE_DIR@');
+var sourceDir = path.resolve(binaryDir, delta);
+
+var cos = require(path.resolve(sourceDir, 'transform/assemble/cosmicos.js'));
+return cos(binaryDir,sourceDir);
