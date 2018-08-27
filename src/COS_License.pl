bfdaf1f2974aeff65736788777a334114c38a660
Paul Fitzpatrick <paul.michael.fitzpatrick@gmail.com>
2018-08-27 18:42:32 -0400

    no need for perl just to add a block of text

--- src/COS_License.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl -w
-
-use strict;
-
-use cosmic;
-
-print ShowLicense();
-
--- src/cosmic.pm
+++ src/cosmic.pm
@@ -656,45 +656,6 @@ sub ShowList {
   return $txt;
 };
 
-
-
-
-
-# generate message
-
-# use consistent random choices
-# (really not acceptable to use random numbers, should use
-# look-up tables on a per lesson basis, to ensure stability
-# of sequences within lessons)
-
-
-sub ShowLicense {
-    
-    my $txt = "#   Author: Paul Fitzpatrick, paulfitz\@csail.mit.edu
-#   Copyright (c) 2005 Paul Fitzpatrick
-#
-#   This file is part of CosmicOS.
-#
-#   CosmicOS is free software; you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation; either version 2 of the License, or
-#   (at your option) any later version.
-#
-#   CosmicOS is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with CosmicOS; if not, write to the Free Software
-#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#
-
-";
-
-    return $txt;
-}
-
 sub ShowLesson {
     my $ltxt = shift;
 
@@ -707,6 +668,11 @@ sub ShowLesson {
     
     print $txt;
 }
+
+# use consistent random choices
+# (really not acceptable to use random numbers, should use
+# look-up tables on a per lesson basis, to ensure stability
+# of sequences within lessons)
 
 srand(1);
 
