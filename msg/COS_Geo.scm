0bfde89035cdd3238d2de40f6494ea41d9196c80
Paul Fitzpatrick <paulfitz@alum.mit.edu>
2019-10-12 08:03:21 -0400

    clean up geo file; fix build from scratch

--- msg/COS_Geo.scm
+++ msg/COS_Geo.scm
@@ -3,36 +3,34 @@
 
 # let us try to make a slightly more interesting world
 
-(define make-table
-  (lambda (lst)
-    (reduce (? x | ? h | 
-	       assign name (car | x) |
-	       assign obj (cdr | x) |
-	       hash-add (h) (name) (obj))
-	    (append (hash-null) (lst)))));
+(define make-table | ? lst |
+   reduce
+     (? x | ? h |
+        assign name (car $x) |
+	assign obj (cdr $x) |
+	hash-add $h $name $obj)
+     (append $hash-null $lst));
 
 # note, the quoted strings below are just represented as a big number,
 # nothing special
-(define geo-map 
-  (make-table
-   (map
-    (? name (cons (name) (room new (name))))
-    (vector "boston" "dublin" "paris" "genoa"))));
+(define geo-map | make-table | map
+   (? name | cons $name | room new $name)
+   (vector "boston" "dublin" "paris" "genoa"));
 
-(define my-links
-  (map 
-   (? entry (assign src (car | entry) |
-		    assign dest (cdr | entry) |
-		    door new (geo-map | src) (geo-map | dest)))
+(define my-links | map
+   (? entry |
+     assign src (car $entry) |
+     assign dest (cdr $entry) |
+     door new (geo-map $src) (geo-map $dest))
    (vector
     (cons "boston" "dublin")
     (cons "dublin" "paris")
     (cons "boston" "paris")
-    (cons "paris" "genoa"))));
+    (cons "paris" "genoa")));
 
-(define myrobo (robo new));
+(define myrobo | robo new);
 
-(myrobo set-room (geo-map "dublin"));
+(myrobo set-room | geo-map "dublin");
 
 (demo | myrobo get-room name);
 
@@ -64,60 +62,44 @@
 # all characters should update together
 
 (class world (the-places the-links)
-       (field things (container new))
-       (field names (cell new (hash-null)))
-       (field places (cell new 0))
-       (field links (cell new 0))
-       (method new 
-	       (begin
-		 (places set
-			(make-table
-			 (map
-			  (? name (cons (name) (room new (name))))
-			  (the-places))))
-		 (links set
-			(map 
-			 (? entry (assign src (car | entry) |
-					  assign dest (cdr | entry) |
-					  door new 
-					  (places get | src) 
-					  (places get | dest)))
-			 (the-links)))))
-       (method add (lambda (place name val) 
-		     (begin
-		       (val set-room (places get | place))
-		       (val set-name | name)
-		       (names set (hash-add (names get)
-					    (name)
-					    (val)))
-		       (things add (val)))))
-       (method find (lambda (n) (names get (n) get-room name)))
-       (method reachable (lambda (place)
-			   (let ((exits
-				  (select-match (lambda (x) 
-						  (instanceof door (x)))
-						(places get (place) inventory))))
-			     (map (? door (door access-from 
-						(places get | place)
-						name))
-				  (exits)))))
-       (method update (begin 
-			(map (? x (x update)) 
-			     (things inventory))
-			(true))));
+   (field things | container new)
+   (field names | cell new $hash-null)
+   (field places | cell new 0)
+   (field links | cell new 0)
+   (method new | begin
+     (places set | make-table | map
+        (? name | cons $name | room new $name)
+	$the-places)
+     (links set | map
+        (? entry |
+          assign src (car $entry) |
+          assign dest (cdr $entry) |
+          door new (places get $src) (places get $dest))
+        $the-links))
+   (method add | lambda (place name val) | begin
+     (val set-room | places get $place)
+     (val set-name $name)
+     (names set | hash-add (names get) $name $val)
+     (things add $val))
+   (method find | ? n  | names get $n get-room name)
+   (method reachable | ? place |
+     assign exits (select-match (instanceof door) | places get $place inventory) |
+     map (? door | door access-from (places get $place) name) $exits)
+   (method update | begin 
+     (map (? x | x update) | things inventory)
+     $true));
 
-(define geo-world
-  (world new 
-	 (vector "boston" "dublin" "paris" "genoa")
-	 (vector
-	  (cons "boston" "dublin")
-	  (cons "dublin" "paris")
-	  (cons "boston" "paris")
-	  (cons "paris" "genoa"))));
+(define geo-world | world new
+   (vector "boston" "dublin" "paris" "genoa")
+     (vector
+       (cons "boston" "dublin")
+       (cons "dublin" "paris")
+       (cons "boston" "paris")
+       (cons "paris" "genoa")));
 
-(geo-world add "dublin" "robo1" (robo new));
+(geo-world add "dublin" "robo1" | robo new);
 
-(geo-world add "genoa" "robo2" (robo new));
+(geo-world add "genoa" "robo2" | robo new);
 
 (demo | geo-world find "robo1");
 (demo | geo-world find "robo2");
