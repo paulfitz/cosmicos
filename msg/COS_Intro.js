08572bec23d1f85498d761f4c2ed689f4319b7be
joha2 <joha2@gmx.net>
2022-04-18 16:12:40 +0200

    message: removed "is"
    - "is" was only used in first section.
    - "is" leads to awkward syntax like `(is int (...))` where the semantics of "is" and "int" are not clear.
    - Removal and introduction of `is-int`, `is-square`, `is-prime`
      simplifies syntax (`is int | unary 1 0` vs. `is-int | unary 1 0`).
    - The semantics of `is-int` is clear: it gets a number and outputs a bool
      and therefore leads to a statement with boolean value (should be true at top level of message).
    - This is in line with the later statements, where e.g. comparisons take place.

--- msg/COS_Intro.js
+++ msg/COS_Intro.js
@@ -5,26 +5,25 @@ var cos = require("./cosmic");
 cos.language(2);
 
 cos.add("intro unary");
-cos.add("intro is");
-cos.add("intro int");
+cos.add("intro is-int");
 for (var i=0; i<16; i++) {
     var ones = "";
     for (var j=0; j<i; j++) {
 	ones += " 1";
     }
-    cos.add("is int | unary" + ones + " 0");
+    cos.add("is-int | unary" + ones + " 0");
 }
 
-cos.add("intro square");
+cos.add("intro is-square");
 for (var i=0; i<6; i++) {
     var ones = "";
     for (var j=0; j<i*i; j++) {
 	ones += " 1";
     }
-    cos.add("is square | unary" + ones + " 0");
+    cos.add("is-square | unary" + ones + " 0");
 }
 
-cos.add("intro prime");
+cos.add("intro is-prime");
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
 for (var i=0; i<primes.length; i++) {
     var prime = primes[i];
@@ -32,5 +31,5 @@ for (var i=0; i<primes.length; i++) {
     for (var j=0; j<prime; j++) {
 	ones += " 1";
     }
-    cos.add("is prime | unary" + ones + " 0");
+    cos.add("is-prime | unary" + ones + " 0");
 }
