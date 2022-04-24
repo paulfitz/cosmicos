0a2ee535cdd2f5b329988a12ea920c243be7ddc5
joha2 <joha2@gmx.net>
2022-04-24 12:23:11 +0200

    analysis: added entropy tests

--- analysis/tests_analysis.py
+++ analysis/tests_analysis.py
@@ -53,3 +53,48 @@ class TestMessagesContainEveryCharacter(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+
+class TestMessagesEntropy(unittest.TestCase):
+
+    def setUp(self):
+        self.d = DecoderClass(logging.getLogger("entropy test"))
+
+    def test_empty_text_word_lengths(self):
+        # empty message and zero word length gives empty list
+        assert len(self.d.performStatistics("", "0123", maxlen=0)) == 0
+        # some message and zero word length gives empty list
+        assert len(self.d.performStatistics("0000", "0123", maxlen=0)) == 0
+        # empty message and non-zero word length gives non-empty list
+        assert len(self.d.performStatistics("", "0123", maxlen=10)) == 10
+
+    def test_zero_entropy(self):
+        for i in range(NUM_TESTS):
+            text_length = random.randint(0, 100)
+            max_length = random.randint(0, 20)
+            statistics = self.d.performStatistics("0"*text_length,
+                                                  "0123",
+                                                  maxlen=max_length)
+            # check numbers of word lengths
+            assert(tuple([number for (number, _) in statistics]) ==\
+                   tuple(range(1, max_length+1)))
+            # check entropy zero
+            assert all([abs(value) < 1e-15 for (_, value) in statistics])
+
+    def test_high_entropy(self):
+        for i in range(NUM_TESTS):
+            text_length = random.randint(1000, 10000)
+            (_, text, _) = self.d.generateRandomMessage(limit=text_length)
+            max_length = random.randint(0, 20)
+            statistics = self.d.performStatistics(text,
+                                                  "0123",
+                                                  maxlen=max_length)
+            # check numbers of word lengths
+            assert(tuple([number for (number, _) in statistics]) ==\
+                   tuple(range(1, max_length+1)))
+            # check entropy > 0.95
+            assert all([abs(value) > 0.95 for (_, value) in statistics])
+
+
+    def tearDown(self):
+        pass
